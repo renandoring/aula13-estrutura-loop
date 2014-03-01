6d65ab8197dbdb0ff9af81a44c33de9e03067b13
rdoring <rdoring@rdoring.(none)>
2014-03-01 19:44:13 -0300

    aula13

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+//$indice = 0;
+//$indice = TRUE;
+$valor = 10;
+
+//while($indice==TRUE){
+    ////echo $indice."\n";
+    ////$indice++;
+    //if($valor<=100){
+        //echo $valor;
+        //$valor += 10;
+        //echo "\n";
+    //}else{
+        //$indice = FALSE;
+    //}
+//}
+
+for($i=0; $i<=100;$i++){
+    if($valor<=100){
+        echo $valor."\n";
+        $valor += 10;
+    }
+}
+?>
